b4a19cac6e9747aa7b2ead530d55f8c944904164
Sandra Cardona Ocampo <sami-1780@hotmail.com>
2025-03-06 18:50:43 -0800

    add rectangle

--- POOConcepts/Taller 2 Herencia/Taller 2 Concepts/Rectangle.cs
+++ POOConcepts/Taller 2 Herencia/Taller 2 Concepts/Rectangle.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Taller_2_Concepts
 {
-    class Rectangle
+    public class Rectangle : Square
     {
+        private int _b;
+
+        public Rectangle(string name, int a, int b) : base(name, a)
+        {
+            _b = b;
+        }
+
+        public int B { get => _b; set => _b = ValidateB(value); }
+
+        public override double GetArea()
+        {
+            return A * B; // Fórmula del área de rectangulo: Base * Altura
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * (A + B); // Fórmula del perímetro: 2 * (base + altura)
+        }
+
+        private int ValidateB(int b)
+
+        {
+            if (b <= 0)
+            {
+                throw new Exception($"The base {b}, must be a positive value.");
+            }
+            return b;
+        }
     }
 }
