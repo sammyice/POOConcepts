8a2bc55d68e060414805d2c89bd10408e88ddad8
Sandra Cardona Ocampo <sami-1780@hotmail.com>
2025-03-06 19:13:52 -0800

    add kite

--- POOConcepts/Taller 2 Herencia/Taller 2 Concepts/Kite.cs
+++ POOConcepts/Taller 2 Herencia/Taller 2 Concepts/Kite.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Taller_2_Concepts
 {
-    class Kite
+    public class Kite : Rhombus
     {
+        private int _b;
+
+        public Kite(string name, int a, int d1, int d2, int b) : base(name, a, d1, d2)
+        {
+            _b = b;
+        }
+
+
+        public int B { get => _b; set => _b = ValidateB(value); }
+
+        public override double GetArea()
+        {
+            return (D1 * D2) / 2; // Utiliza las diagonales para calcular el área (heredadas de Rhombus)
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * (A + B);  // Perímetro basado en lados y valores adicionale
+        }
+
+        private int ValidateB(int b)
+
+        {
+            if (b <= 0)
+            {
+                throw new Exception($"The value for {b}, must be a positive value.");
+            }
+            return b;
+        }
     }
 }
