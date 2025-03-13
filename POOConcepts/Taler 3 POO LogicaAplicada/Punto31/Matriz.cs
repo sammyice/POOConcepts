2c8288a329509c5623f0a1262acefce6c4e9fe64
Sandra Cardona Ocampo <sami-1780@hotmail.com>
2025-03-12 18:33:30 -0700

    created code

---
+++ POOConcepts/Taler 3 POO LogicaAplicada/Punto31/Matriz.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangularInferiorMatriz
+{
+    class Matriz
+    {
+
+        private int[,] _matriz; //Almacenará la matriz
+        private int _n;
+
+        // Constructor por defecto, inicializa la matriz con un tamaño de 10
+        public Matriz()
+        {
+            _n = 10;
+            _matriz = new int[_n, _n];
+        }
+
+        public Matriz(int n)
+        {
+            _n = ValidateN(n); // Validamos el tamaño
+            _matriz = new int[_n, _n]; // Creamos la matriz con el tamaño validado
+        }
+
+        public int N
+        {
+            get => _n;
+            set => _n = ValidateN(value); // Validamos al establecer un nuevo tamaño
+        }
+
+
+        // Método para validar el tamaño de la matriz
+        public int ValidateN(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("El número ingresado no es válido. Por favor, ingrese un número positivo.");
+            }
+            return n;
+        }
+
+        // Método para llenar la matriz con la fórmula: Celda[i][j] = i + j
+        public void LlenarMatriz()
+        {
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    _matriz[i, j] = i + j; // Llenar cada celda con la fórmula
+                }
+            }
+        }
+
+        // Método para mostrar la matriz completa
+        public void MostrarMatrizCompleta()
+        {
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    Console.Write($"{_matriz[i, j],3} "); // Mostramos cada valor con 3 espacios
+                }
+                Console.WriteLine(); // Nueva línea al final de cada fila
+            }
+        }
+
+        // Método para mostrar la triangular inferior de la matriz
+        public void MostrarTriangularInferior()
+        {
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j <= i; j++) // Solo mostramos donde j <= i
+                {
+                    Console.Write($"{_matriz[i, j],3} "); // Mostramos cada valor con 3 espacios
+                }
+                Console.WriteLine(); // Nueva línea al final de cada fila
+            }
+        }
+    }
+}
---
+++ POOConcepts/Taler 3 POO LogicaAplicada/Punto31/Program.cs
@@ -0,0 +1,26 @@
+﻿using TriangularInferiorMatriz;
+
+try
+{
+    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+    Console.Clear();
+    Console.WriteLine("**** TRIANGULAR INFERIOR DE UNA MATRIZ ****");
+    Console.Write("Ingrese orden de la matriz: ");
+
+    int n = int.Parse(Console.ReadLine());
+
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.BackgroundColor = ConsoleColor.DarkGray;
+
+    // Crear instancia de la clase MatrizCuadrada
+    Matriz matriz = new Matriz(n);
+
+    // Llenar y mostrar la matriz
+    matriz.LlenarMatriz();
+    matriz.MostrarMatrizCompleta();
+    matriz.MostrarTriangularInferior();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
